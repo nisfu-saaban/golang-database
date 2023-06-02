facfa84098029f3f22a921b30da9b96a4eb63e79
nisfu-saaban <nisthy07@gmail.com>
2023-06-03 06:49:24 +0700

    sql nullable

--- database_connection_test.go
+++ database_connection_test.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"testing"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -32,4 +33,59 @@ func TestExecuteQuery(t *testing.T) {
 	}
 
 	fmt.Println("Success Insert Data to Database")
+}
+
+func TestQueryContext(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	query := "SELECT id,name FROM customer"
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("id", id)
+		fmt.Println("name", name)
+	}
+
+}
+
+func TestQueryComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	query := "SELECT id,name,email,balance,rating,birth_date,married,created_at FROM customer"
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		var email sql.NullString
+		var balance int32
+		var rating float64
+		var birthDate sql.NullTime
+		var createdAt time.Time
+		var married bool
+		err = rows.Scan(&id, &name, &email, &balance, &rating, &birthDate, &married, &createdAt)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("id", id, "name", name, "email", email, "balance", balance, "rating", rating, "birth date", birthDate, "married", married, "create at", createdAt)
+
+	}
 }
